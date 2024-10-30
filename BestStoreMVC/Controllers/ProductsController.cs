d04c87aeb41d2629c2009f91ea55f38ac02e4d0c
Azizul Arif <azizularif94@gmail.com>
2024-10-30 23:33:07 +0600

    init

---
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderByDescending(p=>p.Id).ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
