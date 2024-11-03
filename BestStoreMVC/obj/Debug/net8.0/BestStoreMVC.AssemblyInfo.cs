23bb0591ca5874050a114a3c37b6eba1bd9aa9fd
Azizul Arif <azizularif94@gmail.com>
2024-11-03 17:05:18 +0600

    layout fix

--- BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
+++ BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6dcdec2c997cee3d59904aac03ca073c697fe4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4fee0fbd27732552918046e29189ee6cbb7d8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
