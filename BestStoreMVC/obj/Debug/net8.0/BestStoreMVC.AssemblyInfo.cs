932a975f63c0e88c83003b46c4504d78350b3337
Azizul Arif <azizularif94@gmail.com>
2024-11-05 10:00:00 +0600

    identity framework

--- BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
+++ BestStoreMVC/obj/Debug/net8.0/BestStoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c4afebd0ffa9ca766748a2b65923fd7bdffb992")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c94afff126cfa5d67461c9c88947ba983c319d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BestStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
